7205707f48e27304e11f62edfc4d4d59733ad840
Emi-J-G <49929524@est.ort.edu.ar>
2026-07-17 13:58:10 -0300

    Update Album.cs

--- Models/Album.cs
+++ Models/Album.cs
@@ -5,14 +5,7 @@ public class Album
     public int id {get; set;}
     public int idFigurita {get; set;}
     public int cantidad {get; set;}
-    private List<Figuritas> pegadas = new List<Figuritas>();
-    
     
     public Album() {
     }
-
-    public List<Figuritas> getPegadas() {
-        return pegadas;
-    }
-    
 }
